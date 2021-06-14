729fcbd6dc34a5ab69e5f12dbabae53c36a59260
Yutian Zhang <yutian.zhang@student.keystoneacademy.cn>
2021-06-14 20:14:16 +0800

    did a little bit

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ human = Human('JerryTNT')
 
 bet = human.place_bet()
 # Draw two cards from the Deck and add to the user's hand (check the methods on the Deck class for help :) )
-
+playercard = deck[0] and deck[1]
+deck -= playercard
+hand = []
+hand += playercard
 # Show the user their current cards and ask them if they want to hit_or_stand
+print(hand)
 
 # If the user decides to HIT, add another card to their hand
 # If the user decides to STAND, end the loop (we will change this next week)
